e884ed3ce37cf63db44753cd04b780243c2f6755
RachelNS <rachelnelsonschille@gmail.com>
2020-12-01 16:40:25 -0800

    submit prework assignments

---
+++ PREWORK_RNS/Module-6/javascript.js
@@ -0,0 +1,19 @@
+    <script type="text/javascript" src="javascript.js">
+    document.getElementById ("Grow").addEventListener ("click", function (){
+        document.getElementById ("box").style.height = "300px"});
+
+    document.getElementById ("Blue") .addEventListener ("click", function () {
+        document.getElementById ("box").style.color = "blue"});
+
+    document.getElementById ("Fade") .addEventListener ("click", function () {
+        document.getElementById ("box").style.opacity = "0.5"}); 
+    
+    document.getElementById ("Reset") .addEventListener ("click", function () {
+        document.getElementById ("box").style.height = "150px"});
+    document.getElementById ("Reset") .addEventListener ("click", function () {
+        document.getElementbyID ("box").style.color = "orange"});
+    document.getElementById ("box") .addEventListener ("click", function () {
+        document.getElementById ("box").style.opacity="1"});   
+
+
+    </script>
